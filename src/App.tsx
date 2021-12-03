0a243476948da3b71c9ed0238a4e9c1e5bd88ac4
Wandy Santana <sntwandy@gmail.com>
2021-12-02 22:59:05 -0400

    Init App

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/**
+ *
+ * @returns {React.ReactElement}
+ */
+
+import { ReactElement } from 'react'
+
+const App = (): ReactElement => {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+/**
+ * @return {ReactDOM.Renderer}
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
