185d8671a690d35a5a2f2ed536a79d16bee0b35c
Wandy Santana <sntwandy@gmail.com>
2021-12-02 23:46:26 -0400

    Configuration for Prettier and Husky

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@
  * @return {ReactDOM.Renderer}
  */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+  document.getElementById('root')
+)
